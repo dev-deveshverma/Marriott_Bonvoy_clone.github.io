3545cef600d18a3b90756b5258a2311fccdaed2a
Your Name <shailendraftp0001@gmail.com>
2022-03-23 21:14:46 +0530

    adde

--- Sai_Nikhil/scripts/conformation.js
+++
@@ -1,3 +0,0 @@
-let random=document.getElementById("orderId");
-let value=Math.ceil((Math.random()*6)*10000);
-random.textContent=`OrderId:${value}`;
--- Sai_Nikhil/src/config/db.js
+++
@@ -1,6 +0,0 @@
-const mongoose=require("mongoose");
-require("dotenv").config();
-const connect=()=>{
-   mongoose.connect(process.env.SERVER);
-};
-module.exports=connect;
